da4df33382bbaa34b23fe1ae887fb07c16fe56a7
Pierre Boislève <pierre.boisleve@gmail.com>
2021-04-12 22:28:47 +0200

    Update Code

--- lecture.c
+++ lecture.c
@@ -7,9 +7,9 @@ absorp lecture(FILE* file_pf, int* file_state){
 
 	int ACr=0, DCr=0, ACir=0, DCir=0;
 	*file_state= fscanf(file_pf, "%d,%d,%d,%d", &ACr,&DCr,&ACir,&DCir);
-    myAbsorp.acr=ACr;
+    myAbsorp.acr=ACr-2048;
     myAbsorp.dcr=DCr;
-    myAbsorp.acir=ACir;
+    myAbsorp.acir=ACir-2048;
     myAbsorp.dcir=DCir;
 	return myAbsorp; // return EOF flag
 
